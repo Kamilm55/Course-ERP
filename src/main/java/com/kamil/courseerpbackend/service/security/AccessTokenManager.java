d11bec3ccbe7bddf571b4fe0e986f04830feb204
Kamilm55 <kamilmmmdov2905@gmail.com>
2023-11-07 12:47:37 +0400

    cannot configure security jwt

--- src/main/java/com/kamil/courseerpbackend/service/security/AccessTokenManager.java
+++ src/main/java/com/kamil/courseerpbackend/service/security/AccessTokenManager.java
@@ -2,14 +2,23 @@ package com.kamil.courseerpbackend.service.security;
 
 import com.kamil.courseerpbackend.model.entity.User;
 import com.kamil.courseerpbackend.service.base.TokenGenerator;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class AccessTokenManager implements TokenGenerator<User> {
     @Override
     public String generate(User obj) {
-        return null;
+        return Jwts.builder()
+                .setClaims(null)
+                .setExpiration(null)
+                .setIssuedAt(null)
+                .setSubject(null)
+//                .signWith()
+                .compact();
     }
 }
