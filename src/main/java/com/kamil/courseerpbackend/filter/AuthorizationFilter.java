5b7aaeafa09c05ee1f5890f2796f74c9c5db6236
Kamilm55 <kamilmmmdov2905@gmail.com>
2023-11-14 14:20:37 +0400

    1:02:38 continue video and practise

--- src/main/java/com/kamil/courseerpbackend/filter/AuthorizationFilter.java
+++ src/main/java/com/kamil/courseerpbackend/filter/AuthorizationFilter.java
@@ -14,9 +14,10 @@ public class AuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // todo: Complete filter logic , it accepts all requests
-//        if this  filterChain.doFilter calls it forward other filer or if there is no filter it forwards to controller
+        // todo: Complete filter logic , now  it accepts all requests
+
         filterChain.doFilter(request,response);//if we don't call this method request don't go to the controller we are not able to show result,
+        //  if this  filterChain.doFilter calls it forward other filer or if there is no filter it forwards to controller
         // for ex: i cannot open swagger or i cannot show even static data  from testController
     }
 }
--- src/main/java/com/kamil/courseerpbackend/service/user/UserDetailsServiceImpl.java
+++ src/main/java/com/kamil/courseerpbackend/service/user/UserDetailsServiceImpl.java
@@ -1,18 +1,36 @@
 package com.kamil.courseerpbackend.service.user;
 
+import com.kamil.courseerpbackend.model.entity.User;
+import com.kamil.courseerpbackend.model.security.LoggedInUserDetails;
+import com.kamil.courseerpbackend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 //    The authentication of any request mostly depends on the implementation of the UserDetailsService interface.
 
+    private final UserService userService;
+    // todo: currently our system supports only email login, but in the future
+    //  we have to implement phone number version
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // todo: complete this logic , we must accept email and return UserDetails
-        return null;
+        // todo: authorities is empty list , we must change this authorities(roles) list
+        User user = userService.getUserByEmail(username);
+
+        if(user!=null){
+            return new LoggedInUserDetails(user.getEmail(),user.getPassword(),new ArrayList<>());
+        }else {
+            throw new RuntimeException("There is no authenticated user with this email");
+        }
+
+
     }
 }
