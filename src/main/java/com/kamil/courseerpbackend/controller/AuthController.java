138a497fab48258e4c8464bcc7766de0a7fe82a7
Kamilm55 <kamilmmmdov2905@gmail.com>
2023-11-18 08:22:38 +0400

    refactor login

--- src/main/java/com/kamil/courseerpbackend/controller/AuthController.java
+++ src/main/java/com/kamil/courseerpbackend/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.kamil.courseerpbackend.model.dto.RefreshTokenDto;
 import com.kamil.courseerpbackend.model.entity.User;
 import com.kamil.courseerpbackend.model.payload.auth.LoginPayload;
 import com.kamil.courseerpbackend.model.response.auth.LoginResponse;
+import com.kamil.courseerpbackend.service.auth.AuthBusinessService;
 import com.kamil.courseerpbackend.service.security.AccessTokenManager;
 import com.kamil.courseerpbackend.service.security.RefreshTokenManager;
 import com.kamil.courseerpbackend.service.user.UserService;
@@ -22,49 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AccessTokenManager accessTokenManager;
-    private final RefreshTokenManager refreshTokenManager;
+    private final AuthBusinessService authBusinessService;
 
     @PostMapping("/login")
     public BaseResponse<LoginResponse> login(@RequestBody LoginPayload payload){
-
-
-        authenticate(payload); // if success continue else throw error
-
-        User user = userService.getUserByEmail(payload.getEmail());
-
-        return BaseResponse.success(
-                LoginResponse.builder()
-                        .accessToken(accessTokenManager.generate(user))
-                        .refreshToken(refreshTokenManager.generate(
-                                RefreshTokenDto.builder().rememberMe(payload.isRememberMe()).user(user).build()
-                        ))
-//                        .userInfo()
-                        .build()
-        );
+        return BaseResponse.success(authBusinessService.login(payload));
     }
 
-    // temp method , todo: refactor this
-
-    private final AuthenticationManager authenticationManager;
-    private final UserService userService;
-    private void authenticate(LoginPayload request){
-
-        try{
-            authenticationManager.authenticate(
-                    //todo: add authorities to constructor as 3rd parameter
-                    //Learn:
-                    // This check is there any user with this credentials or not
-                    // this is my payload password:request.getPassword()
-                    // and in config we set UserDetails which is fetched from db ,
-                    // there is also my (userDetails) password associated with this username(email)
-                    new UsernamePasswordAuthenticationToken(request.getEmail() ,request.getPassword())
-            );
-        }
-        catch (AuthenticationException ex){
-            throw new RuntimeException(ex);
-        }
-
-    }
 
 }
---
+++ src/main/java/com/kamil/courseerpbackend/service/auth/AuthBusinessServiceImpl.java
@@ -0,0 +1,66 @@
+package com.kamil.courseerpbackend.service.auth;
+
+import com.kamil.courseerpbackend.model.base.BaseResponse;
+import com.kamil.courseerpbackend.model.dto.RefreshTokenDto;
+import com.kamil.courseerpbackend.model.entity.User;
+import com.kamil.courseerpbackend.model.payload.auth.LoginPayload;
+import com.kamil.courseerpbackend.model.response.auth.LoginResponse;
+import com.kamil.courseerpbackend.service.security.AccessTokenManager;
+import com.kamil.courseerpbackend.service.security.RefreshTokenManager;
+import com.kamil.courseerpbackend.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthBusinessServiceImpl implements AuthBusinessService{
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final AccessTokenManager accessTokenManager;
+    private final RefreshTokenManager refreshTokenManager;
+
+    @Override
+    public LoginResponse login(LoginPayload payload) {
+        authenticate(payload); // if success continue else throw error
+
+        User user = userService.getUserByEmail(payload.getEmail());
+
+        return     LoginResponse.builder()
+                        .accessToken(accessTokenManager.generate(user))
+                        .refreshToken(refreshTokenManager.generate(
+                                RefreshTokenDto.builder().rememberMe(payload.isRememberMe()).user(user).build()
+                        ))
+                    .build();
+//                        .userInfo()
+        //todo: add user info
+
+    }
+
+    // private util method
+    private void authenticate(LoginPayload request){
+
+        try{
+            authenticationManager.authenticate(
+                    //todo: add authorities(selahiyet) to constructor as 3rd parameter
+
+                    //Learn:
+                    // This check is there any user with this credentials or not
+                    // this is my payload password:request.getPassword()
+                    // and in config we set UserDetails which is fetched from db ,
+                    // there is also my (userDetails) password associated with this username(email)
+                    new UsernamePasswordAuthenticationToken(request.getEmail() ,request.getPassword())
+            );
+        }
+        catch (AuthenticationException ex){
+            // todo: add custom exception
+            throw new RuntimeException(ex);
+        }
+
+    }
+
+}
